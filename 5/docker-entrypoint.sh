507e05a57daa8699513e476fd5819642fa37003f
edward-rojas <edward.rojas@fr.ibm.com>
2016-12-15 15:52:43 +0100

    Adding elasticsearch to root group to be able to acces to volume and Install head plugin by default

--- 5/docker-entrypoint.sh
+++ 5/docker-entrypoint.sh
@@ -11,7 +11,9 @@ fi
 # allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+	# chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+	adduser elasticsearch root
+	chmod 775 /usr/share/elasticsearch/data
 	
 	set -- gosu elasticsearch "$@"
 	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
